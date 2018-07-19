1318c3cc402427bf1ad77d004de648962be7b346
Daniel Velez <velez.a.daniel@gmail.com>
2018-07-19 01:06:08 -0400

    updated read me

--- src/containers/Inbox/chat.js
+++ src/containers/Inbox/chat.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-
+import {View, StyleSheet, TextInput, Image, ScrollView} from 'react-native';
+import Text from '../../components/forms/Text';
+import MainContainer from '../../components/mainContainer';
+import {simpleLineIcons} from '@expo/vector-icons';
+import data from '../../data/chatData'
 
 
 export default class Chat extends React.Component {
